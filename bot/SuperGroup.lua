768c4ad52e9d56745e755044676ed42b2d099592
rezaeblisam <rezaeblisam@gmail.com>
2016-06-11 00:21:10 -0700

    Update SuperGroup.lua

--- bot/SuperGroup.lua
+++ bot/SuperGroup.lua
@@ -235,7 +235,7 @@ function create_config( )
         "plugins"
         
     },
-    sudo_users = {88351205},--Sudo users
+    sudo_users = {188351205},--Sudo users
     moderation = {data = 'data/moderation.json'},
     about_text = [[Put Everything You Like :) ( Don t Forgot Edit plugins/stats.lua]],
     help_text_realm = [[
